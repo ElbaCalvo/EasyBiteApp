07546f96298efef60f664ea8df77c2701c907564
Elba Calvo <ecalvog@fpcoruna.afundacion.org>
2024-05-04 19:49:06 +0200

    Añadido post de una receta en el mmealplan del usuario (sin probar)

--- easybite-backend/easybitebackend/easybitebackendapp/endpoints.py
+++ easybite-backend/easybitebackend/easybitebackendapp/endpoints.py
@@ -7,7 +7,7 @@ from django.db.models import F, ExpressionWrapper, FloatField
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
-from .models import User, UserSession, Ingredients, Recipes, UserFavorites
+from .models import User, UserSession, Ingredients, Recipes, UserFavorites, UserMealPlan
 
 def authenticate_user(request):
     session_token = request.headers.get('SessionToken', None)
@@ -205,4 +205,25 @@ def delete_user_favorite(request, recipe_id):
         except PermissionDenied:
             return JsonResponse({'response': 'unauthorized'}, status=401)
         except UserFavorites.DoesNotExist:
+            return JsonResponse({'response': 'not_found'}, status=404)
+
+@csrf_exempt
+def user_mealplan(request, day):
+    if request.method == 'POST':
+        try:
+            user_session = authenticate_user(request)
+            user = User.objects.get(id=user_session.user.id)
+            data = json.loads(request.body)
+            recipe_id = data.get('recipe_id') 
+            recipe = Recipes.objects.get(id=recipe_id)
+            valid_days = [choice[0] for choice in UserMealPlan.WEEKDAYS]
+            if day not in valid_days:
+                return JsonResponse({'response': 'not_ok'}, status=400)
+            UserMealPlan.objects.create(user=user, recipe=recipe, week_day=day)
+            return JsonResponse({'response': 'ok'}, status=201)
+        except PermissionDenied:
+            return JsonResponse({'response': 'unauthorized'}, status=401)
+        except User.DoesNotExist:
+            return JsonResponse({'response': 'not_found'}, status=404)
+        except Recipes.DoesNotExist:
             return JsonResponse({'response': 'not_found'}, status=404)
