420db8cfa514a4f2f6ef3b03918f2761612b44f4
Elba Calvo García <ecalvog@fpcoruna.afundacion.org>
2024-05-15 23:23:17 +0200

    Creado y avanzado en el XML de la pantalla de inicio de sesion

--- easybite-app/app/build.gradle.kts
+++ easybite-app/app/build.gradle.kts
@@ -29,7 +29,9 @@ android {
 }
 
 dependencies {
-
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
---
+++ easybite-app/app/src/main/java/com/example/easybite/Server.java
@@ -0,0 +1,5 @@
+package com.example.easybite;
+
+public class Server {
+    public static String name = "http://10.0.2.2:8000";
+}
