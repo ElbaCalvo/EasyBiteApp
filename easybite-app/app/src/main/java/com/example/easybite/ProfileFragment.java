9c4d54a5c60c4c120411cf7fb9eda86f51fc1645
Elba Calvo <ecalvog@fpcoruna.afundacion.org>
2024-05-27 22:53:46 +0200

    Añadida pantalla de editar perfil y botones de cerrar sesion y eliminar cuenta

--- easybite-app/app/src/main/java/com/example/easybite/ProfileFragment.java
+++ easybite-app/app/src/main/java/com/example/easybite/ProfileFragment.java
@@ -1,7 +1,10 @@
 package com.example.easybite;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +39,7 @@ import java.util.List;
 
 public class ProfileFragment extends Fragment {
     private TextView usernameLabel, emailLabel, birthdateLabel;
+    private Button closeSession, deleteAccount;
     private RequestQueue queue;
     private List<RecipesData> recipesDataList;
     private RecipesAdapter adapter;
@@ -46,10 +51,11 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        // Inicializar vistas
         usernameLabel = view.findViewById(R.id.username_label);
         emailLabel = view.findViewById(R.id.email_label);
         birthdateLabel = view.findViewById(R.id.birthdate_label);
+        closeSession = view.findViewById(R.id.close_button);
+        deleteAccount = view.findViewById(R.id.delete_button);
 
         recyclerView = view.findViewById(R.id.recycler_view_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -61,8 +67,60 @@ public class ProfileFragment extends Fragment {
         context = getContext();
 
         view.findViewById(R.id.edit_profile).setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), DetailActivity.class);
+            Intent intent = new Intent(v.getContext(), EditProfileActivity.class);
             context.startActivity(intent);
+        });
+
+        closeSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = context.getSharedPreferences("EASYBITE_APP_PREFS", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("¿Estás seguro de que deseas eliminar tu cuenta?");
+                builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        JsonObjectRequestWithAuthentication request = new JsonObjectRequestWithAuthentication(
+                                Request.Method.DELETE,
+                                "/user",
+                                null,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        Intent intent = new Intent(context, LoginActivity.class);
+                                        startActivity(intent);
+                                        getActivity().finish();
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Toast.makeText(getContext(), "Error al eliminar la cuenta", Toast.LENGTH_SHORT).show();                                    }
+                                },context
+                        );
+                        queue.add(request);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
         });
 
         return view;
