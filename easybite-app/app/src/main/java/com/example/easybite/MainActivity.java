68f4090ea1a7c573a7899b2fadefee26285f69fb
Elba Calvo <ecalvog@fpcoruna.afundacion.org>
2024-05-27 08:42:09 +0200

    Añadida bottom navigation bar (aun sin páginas linkeadas) y también añadida toolbar

--- easybite-app/app/src/main/java/com/example/easybite/MainActivity.java
+++ easybite-app/app/src/main/java/com/example/easybite/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.easybite;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.easybite.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_acivity, new PrincipalFragment())
-                    .commit();
-        }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        navView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_SELECTED);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_profile, R.id.navigation_home, R.id.navigation_profile).build();
+
+        NavController navController = Navigation.findNavController(this,
+                R.id.nav_host_fragment_activity_main);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
